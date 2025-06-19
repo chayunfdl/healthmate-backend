536e096e822faa275b987df3b4e8419cc7c0b77e
Chayun Fadila <128894491+chayunfdl@users.noreply.github.com>
2025-06-19 22:42:53 +0700

    fix: update start file for Render deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Mengizinkan Cross-Origin Resource Sharing
